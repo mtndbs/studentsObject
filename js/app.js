d9e1c81450107595de9ed9801b1d8aa2dd78be07
mtndbs <mtndbs@gmail.com>
2022-07-17 17:51:28 +0300

    now Its working

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 let studentList = [];
-let studentId = {
-  name: " ",
-  proffetion: " ",
-  age: " ",
-  car: { type: " ", color: "", year: "" },
-};
+
 
 function studentName(){
+  let studentId = {
+    name: " ",
+    proffetion: " ",
+    age: " ",
+    car: { type: " ", color: "", year: "" },
+  };
+
 studentId.name = document.querySelector('#student-name').value
 studentId.proffetion = document.querySelector('#student-prof').value
 studentId.age = document.querySelector('#student-age').value
@@ -21,15 +23,17 @@ console.log(studentList);
 }
 
 
-
 function displayStudent(){
-    document.querySelector("#scrn").innerHTML = "<ul>";
-    document.querySelector("#scrn").innerHTML += '<li> Name:'+studentId.name+'</li>'
-    document.querySelector("#scrn").innerHTML += '<li> Prof:'+studentId.proffetion+'</li>'
-    document.querySelector("#scrn").innerHTML += '<li> Age:'+studentId.age+'</li>'
+  // console.log(studentList.at(-1).name)
+  for(student of studentList){
+  document.querySelector("#scrn").innerHTML += "<ul>";
+    document.querySelector("#scrn").innerHTML += '<li> Name:'+student.name+'</li>'
+    document.querySelector("#scrn").innerHTML += '<li> Prof:'+student.proffetion+'</li>'
+    document.querySelector("#scrn").innerHTML += '<li> Age:'+student.age+'</li>'
     document.querySelector("#scrn").innerHTML += '<li> Car:</li>'
-    document.querySelector("#scrn").innerHTML += '   <li> type:'+studentId.car.type+'</li>'
-    document.querySelector("#scrn").innerHTML += '   <li> color: <span style="background-color:'+studentId.car.color+';"> Car color </span></li>'
-    document.querySelector("#scrn").innerHTML += '   <li> year:'+studentId.car.year+'</li>'
+    document.querySelector("#scrn").innerHTML += '   <li> type:'+student.car.type+'</li>'
+    document.querySelector("#scrn").innerHTML += '   <li> color: <span style="background-color:'+student.car.color+';"> Car color </span></li>'
+    document.querySelector("#scrn").innerHTML += '   <li> year:'+student.car.year+'</li>'
   document.querySelector("#scrn").innerHTML += "</ul>";
+}
 }
